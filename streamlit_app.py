63a295dd01581b3a19da1424191fa8dc93287b3c
4evLearner <113939883+4evLearner@users.noreply.github.com>
2022-09-19 21:45:21 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit
 
 streamlit.title("Hello world")
-streamlit.header('🥣 Breakfast Menu')
-streamlit.text('🥗 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🐔 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+streamlit.header(' Breakfast Favorite')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
